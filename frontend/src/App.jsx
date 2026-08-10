bbdc5085dee33d9e10d589524b4a438c6bb503d7
Prasanta Kumar Khuntia <prasantakhuntia2018@gmail.com>
2026-08-10 10:44:05 +0530

    Done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,67 +1,72 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
-// Components
+// Layout Components
 import Topbar from "./Components/Topbar/Topbar";
 import Navbar from "./Components/Navbar/Navbar";
 import Footer from "./Components/Footer/Footer";
 
-// Pages
+// Page Components
 import Home from "./Pages/Home/Home";
 import About from "./Pages/About/About";
 import Blog from "./Pages/Blog/Blog";
-import ContactUs from "./Pages/ContactUs/ContactUs";
-import Price from "./Pages/price/price"; 
-import Courses from "./Pages/Courses/Courses";
-import Shop from "./Pages/Shop/Shop";
-import Faq from "./Pages/Faq/Faq";
-import Teacher from "./Pages/Teacher/Teacher";
-import TeacherDetails from "./Pages/TeacherDetails/TeacherDetails";
-import ClassDetails from "./Pages/ClassDetails/ClassDetails";
 import BlogDetails from "./Pages/BlogDetails/BlogDetails";
-import Termandconditions from "./Pages/Termandconditions/Termandconditions";
-import Privacypolicy from "./Pages/Privacypolicy/Privacypolicy";
-
-
+import Courses from "./Pages/Courses/Courses";
+import ClassDetails from "./Pages/ClassDetails/ClassDetails";
+import Shop from "./Pages/Shop/Shop";
 import Gallery from "./Pages/Gallery/Gallery";
 import Pricing from "./Pages/Pricing/Pricing";
 import TimeTable from "./Pages/TimeTable/TimeTable";
+import Teacher from "./Pages/Teacher/Teacher";
+import TeacherDetails from "./Pages/TeacherDetails/TeacherDetails";
+import Faq from "./Pages/Faq/Faq";
+import ContactUs from "./Pages/ContactUs/ContactUs";
+import Termandconditions from "./Pages/Termandconditions/Termandconditions";
+import Privacypolicy from "./Pages/Privacypolicy/Privacypolicy";
 
 const App = () => {
   return (
     <BrowserRouter>
-      {/* Common Components */}
+      {/* Global Header Elements */}
       <Topbar />
       <Navbar />
 
-      {/* Page Routes */}
+      {/* Application Routes */}
       <Routes>
+        {/* Home Routes (Canonical Home at `/`) */}
         <Route path="/" element={<Home />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/about" element={<About/>} />
+        <Route path="/home" element={<Navigate to="/" replace />} />
+
+        {/* Core Pages */}
         <Route path="/about" element={<About />} />
+        <Route path="/courses" element={<Courses />} />
+        <Route path="/classes/details" element={<ClassDetails />} />
+
+        {/* Teachers / Instructors */}
+        <Route path="/teachers" element={<Teacher />} />
+        <Route path="/teachers/details" element={<TeacherDetails />} />
+
+        {/* Blog & News */}
         <Route path="/blog" element={<Blog />} />
+        <Route path="/blog/:id" element={<BlogDetails />} />
+
+        {/* Utility & Features */}
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/timetable" element={<TimeTable />} />
+        <Route path="/faq" element={<Faq />} />
         <Route path="/contact" element={<ContactUs />} />
-        <Route path="/price" element={<Price />} />
-        <Route path="/Courses" element={<Courses/>} />
-        <Route path="/shop" element={<Shop/>}/>
 
+        {/* SEO-Optimized Legal Slugs */}
+        <Route path="/terms-and-conditions" element={<Termandconditions />} />
+        <Route path="/privacy-policy" element={<Privacypolicy />} />
 
-
-        <Route path="/gallery" element={<Gallery/>}/>
-        <Route path="/pricing" element={<Pricing/>}/>
-        <Route path="/time-table" element={<TimeTable/>}/>  
-        
-
-
-        <Route path="/class-details/2" element={<ClassDetails/>}/>
-        <Route path="/blog/3" element={<BlogDetails/>}/>
-        <Route path="/termandcondition" element={<Termandconditions/>}/>
-        <Route path="/privacypolicy" element={<Privacypolicy/>}/>
-       
+        {/* 404 Fallback for Unmatched Routes */}
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
 
-      {/* Common Footer */}
+      {/* Global Footer Element */}
       <Footer />
     </BrowserRouter>
   );
