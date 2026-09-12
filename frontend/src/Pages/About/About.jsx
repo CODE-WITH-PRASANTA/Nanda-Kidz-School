ec1514470583ebcb02021af6c48197b5a1bb829b
Santanu <santanuranjanbal@gmail.com>
2026-09-12 11:06:12 +0530

    pull

--- frontend/src/Pages/About/About.jsx
+++ frontend/src/Pages/About/About.jsx
@@ -1,22 +1,23 @@
-import React from 'react'
-import AboutUs from '../../Components/AboutUs/AboutUs'
-import AboutBreadCrumb from '../../Components/AboutBreadCrumb/AboutBreadCrumb'
-import AboutOurFacilities from '../../Components/AboutOurFacilities/AboutOurFacilities'
-import AboutBackbone from '../../Components/AboutBackbone/AboutBackbone';
-import AboutKidsSchool from '../../Components/AboutKidsSchool/AboutKidsSchool';
-import AboutContact from '../../Components/AboutContact/AboutContact';
-import AboutDetails from '../../Components/AboutDetails/AboutDetails';
+import React from "react";
+import AboutUs from "../../Components/AboutUs/AboutUs";
+import AboutBreadCrumb from "../../Components/AboutBreadCrumb/AboutBreadCrumb";
+import AboutOurFacilities from "../../Components/AboutOurFacilities/AboutOurFacilities";
+import AboutBackbone from "../../Components/AboutBackbone/AboutBackbone";
+import AboutKidsSchool from "../../Components/AboutKidsSchool/AboutKidsSchool";
+import AboutContact from "../../Components/AboutContact/AboutContact";
+import AboutDetails from "../../Components/AboutDetails/AboutDetails";
+import "./About.css";
 
 const About = () => {
   return (
-    <div>
-        <AboutBreadCrumb/>
-        <AboutUs/>
-        <AboutOurFacilities/>
-        <AboutDetails/>
-        <AboutBackbone/>
-        <AboutKidsSchool/>
-        <AboutContact/>
+    <div className="about-page">
+      <AboutBreadCrumb />
+      <AboutUs />
+      <AboutOurFacilities />
+      <AboutDetails />
+      <AboutBackbone />
+      <AboutKidsSchool />
+      <AboutContact />
     </div>
   );
 };
